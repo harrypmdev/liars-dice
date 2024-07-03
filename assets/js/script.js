8d163296157aee2549bb3c357eb243b1142c5b69
Harry Miles <harrypetermiles@gmail.com>
2024-07-03 16:18:07 +0000

    feat: add player hand to play.html and start functions for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
     accountForHeader();
+    // Find current page and assign to fileName
+    let path = window.location.pathname;
+    let fileName = path.substring(path.lastIndexOf('/') + 1);
+    if (fileName === 'play.html') {
+        startNewGame();
+    }
 });
 
 addEventListener("resize", accountForHeader);
 
+/**
+ * Adjusts the offset of the main section of the page to account
+ * for different header sizes on different devices.
+ * Runs when the page first loads
+ */
 function accountForHeader() {
     let height = document.getElementsByTagName('header')[0].offsetHeight;
     height += 15;
     document.getElementsByTagName('main')[0].style.paddingTop = `${height}px`;
+}
+
+
+/**
+ * Starts a new game of Liar's Dice.
+ * Runs when play.html first loads and every time the game is restarted
+ */
+function startNewGame() {
+
+}
+
+/**
+ * Populates the player or opponents hand with six new dice
+ * Takes one string parameter, 'player-hand' or 'opponent-hand'
+ */
+function populateHand(hand) {
+    let myHand = document.getElementById(hand);
+    
+    myHand.appendChild();
+}
+
+/**
+ * Generates a random number between 1 and 6.
+ * Takes no parameters
+ */
+function generateDiceNumber() {
+    let randomNumber = Math.ceil(Math.random() * 6);
+    return randomNumber;
 }
