79a0e849decccb4ffa0651a279f91fa2d17dda8a
Harry Miles <harrypetermiles@gmail.com>
2024-07-09 22:39:28 +0000

    fix: remove opponent.js and split functions between utility.js and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 import {Bet} from './bet.js';
 import * as utility from './utility.js';
-import * as opponent from './opponent.js';
 
 /* Runs when the page is loaded */
 document.addEventListener("DOMContentLoaded", function() {
@@ -84,40 +83,26 @@ function handleBet(event) {
  * the current quantity.
  */
 function handlePipChange(){
+    console.log("HANDLING PIP CHANGE!");
     let currentBet = document.getElementById('current-bet');
-    // If first turn of the game, exit function
-    if (currentBet.getAttribute('quantity') == 0) {
-        return;
-    }
     let quantitySelector = document.getElementById('quantity-selector');
     let pipSelector = document.getElementById('pip-selector');
-    // Check if the current bet quantity is currently an option of the quantity selector
-    let containsCurrentQuantity = false;
+    let firstTurn = currentBet.getAttribute('quantity') == 0;
+    let current = false;
     for (let option of quantitySelector.options) {
-        if (option.value === currentBet.getAttribute('quantity')) {
-            containsCurrentQuantity = true;
-        }
+	    current = option.value == currentBet.getAttribute('quantity') ? option : false;
     }
-    /* If the currently selected pip is higher than current bet pip value, 
-    add the quantity from current bet to quantity selector
-    */
-    if (parseInt(pipSelector.value) > parseInt(currentBet.getAttribute('pips'))) {
-        // If current bet quantity not an option, add it
-        if (!containsCurrentQuantity) {
-            let newOption = document.createElement('option');
-            newOption.value = currentBet.getAttribute('quantity');
-            newOption.innerHTML = currentBet.getAttribute('quantity');
-            quantitySelector.insertBefore(newOption, quantitySelector.firstChild);
-        }
-    } else {
-        // If current bet quantity is already an option, remove it
-        if (containsCurrentQuantity) {
-            for (option of quantitySelector.options) {
-                if (option.value == currentBet.getAttribute('quantity')) {
-                    quantitySelector.remove(option);
-                } 
-            }
-        }
+    let shouldHaveCurrent = parseInt(pipSelector.value) > parseInt(currentBet.getAttribute('pips'));
+    // If the selector has the current quantity but shouldn't do, remove it
+    if (current && !shouldHaveCurrent && !firstTurn) {
+	    quantitySelector.remove(current);
+    }
+    // If the selector doesn't have the current quantity but should do, add it
+    if (!current && shouldHaveCurrent  && !firstTurn) {            
+	    let newOption = document.createElement('option');
+        newOption.value = currentBet.getAttribute('quantity');
+        newOption.innerHTML = currentBet.getAttribute('quantity');
+        quantitySelector.insertBefore(newOption, quantitySelector.firstChild);
     }
 }
 
@@ -131,7 +116,7 @@ function callGame(caller) {
     let opponentHand = document.getElementById('opponent-hand');
     let currentBet = document.getElementById('current-bet');
     if (caller === 'opponent') {
-        opponent.updateResponseMessage('The computer has called!');
+        utility.updateOpponentResponseMessage('The computer has called!');
     }
     let bothHandsDiceArray = utility.returnDiceArray('player-hand') + utility.returnDiceArray('opponent-hand')
     let allPipsObject = utility.tallyPipTotals(bothHandsDiceArray);
@@ -149,6 +134,84 @@ function callGame(caller) {
     }
     document.getElementById('outcome-text').innerHTML = utility.constructOutcomeText(caller, betIsCorrect, pipOccurrences);
     checkForGameFinish();
+}
+
+/**
+ * Create a bet from the opponent
+ */
+function createResponse() {
+    // Variables that determine the opponent's response style
+    let confidence = 0.47; // How likely the computer is to refrain from calling based on how its dice match the bet - (0 is never, 1 is very likely)
+    let scepticism = 0.51; // How likely the computer is to call based on the overall dice quantity - (0 is never, 1 is very likely)
+    let chanceOfPickingRandom = 0.15; // How likely the computer is to bet a random pip number - (0 is never, 1 is very likely)
+    let chanceOfSame = 0.85; // How likely the computer is pick the same quantity of dice when possible - (0 is never, 1 is very likely)
+    // Choose randomly whether to 'call' or bet
+    let callChanceRandomNum = Math.random();
+    let currentBet = document.getElementById('current-bet');
+    // Find the pip number which occurs the most in the hand, and the second most
+    let totalPipTally = utility.tallyPipTotals(utility.returnDiceArray('opponent-hand'));
+    let highestPip = utility.findHighestPipTotal(totalPipTally)[0];
+    let highestPipOccurrences = utility.findHighestPipTotal(totalPipTally)[1];
+    delete totalPipTally[highestPip];
+    let secondHighestPip = utility.findHighestPipTotal(totalPipTally)[0];
+    let secondHighestPipOccurrences = utility.findHighestPipTotal(totalPipTally)[1];
+    // Make computer more likely to call game if overall quantity of dice is higher
+    let currentQuantity = document.getElementById('current-bet').getAttribute('quantity');
+    if (currentQuantity > 2) {
+        callChanceRandomNum += (currentQuantity-2) * (scepticism/3);
+    }
+    // Make computer less likely to call game if bet matches dice in hand
+    if (currentBet.getAttribute('pips') == highestPip) {
+        callChanceRandomNum -= (highestPipOccurrences*(confidence/3));
+    } else if (currentBet.getAttribute('pips') == secondHighestPip) {
+        callChanceRandomNum -= (secondHighestPipOccurrences*(confidence/3));     
+    }
+    // If opponent is playing first, make the chance of calling zero
+    let lastWinner = false;
+    if (currentBet.getAttribute('last-winner') === "opponent") {
+        callChanceRandomNum = 0;
+        currentBet.setAttribute('last-winner', '');
+        // Make sure a quantity of two is bet later in the function
+        lastWinner = true;
+    }
+    // Randomly call or bet
+    if (callChanceRandomNum > 0.9) {
+        callGame('opponent');
+    } else {
+        // Make bet
+        let newBet = new Bet(0, 0);
+        // Pick the pip number
+        let pipChoiceRandomNum = Math.random();
+        let smartPipChoiceRandomNum = Math.random();
+        // Depending on the 'chanceOfPickingRandom' variables, potentially pick a random number
+        if (pipChoiceRandomNum < chanceOfPickingRandom) {
+            newBet.pips = utility.generateDiceNumber();
+        // Otherwise, pick the highest occuring pip number, second highest, or raise the current bet
+        } else if (smartPipChoiceRandomNum < 0.33) {
+            newBet.pips = highestPip;
+        } else if (smartPipChoiceRandomNum < 0.66) {
+            newBet.pips = secondHighestPip;
+        } else {
+            if (lastWinner) {
+                newBet.pips = highestPip;
+            } else {
+                newBet.pips = currentBet.getAttribute('pips');
+            }
+        }
+        // Pick quantity - either the current quantity if the pip number allows, or higher if not
+        if (newBet.pips > currentBet.getAttribute('pips') && Math.random() < chanceOfSame) {
+            newBet.quantity = currentQuantity;
+        } else {
+            newBet.quantity = parseInt(currentQuantity) + 1;
+        }
+        // Pick 2 if playing the starting bet
+        if (lastWinner) {
+            newBet.quantity = 2;
+        }
+        // Update the board
+        newBet.updateCurrentBet();
+        updateResponseMessage(`The computer has bet ${newBet.quantity} dice with ${newBet.pips} pips.`);
+    }
 }
 
 /**
@@ -218,7 +281,7 @@ function updateBetOptions(){
  * Handles the next turn button
  */
 function handleNextTurn() {
-    opponent.updateResponseMessage("The computer is awaiting your move.");
+    utility.updateOpponentResponseMessage("The computer is awaiting your move.");
     document.getElementById('bet-button').disabled = false;
     document.getElementById('quantity-selector').disabled = false;
     document.getElementById('pip-selector').disabled = false;
